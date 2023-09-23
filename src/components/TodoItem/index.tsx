a688d6b30457fba487bd867d086989c9d1d5ecbf
leticiafarias <leticia_farias@alu.ufc.br>
2023-09-22 21:15:51 -0300

    change colors

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -24,8 +24,8 @@ export function TodoItem({ todo, toggle, remove }: ITodoItem) {
       className={clsx(
         'w-full px-4 py-3 rounded-md border-1 border-gray-600 flex flex-row gap-6 justify-between items-center',
         {
-          'bg-gray-300': !done,
-          'bg-green-400': done,
+          'bg-gray-500': !done,
+          'bg-green-900': done,
         }
       )}
     >
